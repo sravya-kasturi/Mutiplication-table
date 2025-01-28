93d19043c88516ff877e3932a49c3c50e5c405dd
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 15:40:22 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Multiplication table of " + num + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        scanner.close();
+    }
+}
